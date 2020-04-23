cdad6ea1cd7511d7c362894e5e061e70adf0bbdb
Rafael Goulart <faelplg@gmail.com>
2020-04-23 03:34:52 -0300

    Update: Included Css Modules with Sass.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import './App.scss';
+import classes from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={classes.App}>
+      <header className="AppHeader">
+        <h1>fael.tech</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming soon...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
