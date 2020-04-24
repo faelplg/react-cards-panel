22e1281e440c2a5014bf5f26703057e23f9c4ec5
Rafael Goulart <faelplg@gmail.com>
2020-04-24 02:23:03 -0300

    Update: Implementing the project typography.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,37 @@ import classes from './App.module.scss';
 function App() {
   return (
     <div className={classes.App}>
-      <header className="AppHeader">
+      <header className={classes.AppHeader}>
+        <h2 className="text--display-4">fael.tech</h2>
+        <h2 className="text--display-3">fael.tech</h2>
+        <h2 className="text--display-2">fael.tech</h2>
+        <h2 className="text--display-1">fael.tech</h2>
+        <h1 className="text--headline">fael.tech</h1>
+        <h2 className="text--title">fael.tech</h2>
+        <h2 className="text--subtitle">fael.tech</h2>
+        <h3 className="text--menu">fael.tech</h3>
+        <h3 className="text--button">fael.tech</h3>
         <h1>fael.tech</h1>
-        <p>
-          Coming soon...
-        </p>
       </header>
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est necessitatibus praesentium
+          tempore architecto? Necessitatibus, nesciunt laborum obcaecati similique laboriosam iste
+          exercitationem excepturi commodi corporis maiores numquam, voluptatem iusto perferendis!
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit a, optio deserunt,
+          voluptates ex soluta sequi ducimus aspernatur nostrum asperiores nisi. Molestiae sint,
+          saepe aliquam repudiandae dolorum eum adipisci in. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Nostrum accusamus amet quam magnam voluptas quos libero harum nulla, est
+          eveniet tempore cumque dolor labore et beatae quidem illum quibusdam culpa.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est necessitatibus praesentium
+          tempore architecto? Necessitatibus, nesciunt laborum obcaecati similique laboriosam iste
+          exercitationem excepturi commodi corporis maiores numquam, voluptatem iusto perferendis!
+        </p>
+      </main>
     </div>
   );
 }
