ba74dd318e2562bad092fbf05d9de992873bdfe0
Rafael Goulart <faelplg@gmail.com>
2020-06-23 23:56:30 -0300

    Fixing some debugs.

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Panel.module.scss';
 
 const Panel = (props) => {
-  console.log('props', props);
+  console.log('DEBUG: on [Panel].() - props', props);
   return (
     <div className={[classes.Panel, props.shrink ? classes.Shrink : null].join(' ')}>
       {props.children}
